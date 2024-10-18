bcc3cbe11e450f81c52b7b245ba8c9a101d11011
mlorek <michal.lorek@gmail.com>
2024-10-18 13:39:03 +0100

    sql version

--- src/sql/snowflake_tools.sql
+++ src/sql/snowflake_tools.sql
@@ -1,6 +1,7 @@
 --use database <db>;
 --use schema <sch>;
 
+-- Java version
 create or replace function random_uuid()
 returns string
 language java
@@ -11,4 +12,16 @@ class SnowflakeTools{
     return java.util.UUID.randomUUID().toString();
   }
 }
+$$;
+
+-- SQL version
+create or replace function random_uuid_2()
+returns string
+as
+$$
+with h as (
+    select md5(random()) as h
+)
+select substr(h, 1, 8) || '-' || substr(h, 9, 4) || '-' || substr(h, 13, 4) || '-' || substr(h, 17, 4) || '-' || substr(h, 21, 12) as g
+from h
 $$;
